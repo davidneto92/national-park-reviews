f2e666ea99a4a4618c58d8f0ec845441ef1b7f00
David Neto <davidneto92@gmail.com>
2017-05-02 13:59:06 -0400

    Changed review_voting.js from ES6 to ES5...for heroku

--- app/assets/javascripts/review_voting.js
+++ app/assets/javascripts/review_voting.js
@@ -1,11 +1,13 @@
-$(function (){
+// 'use strict';
+
+$(function () {
   // this block modifies the vote buttons to indicate how the
   // current_user has voted
   $.ajax({
     method: 'GET',
     url: '/api/v1/review_votes',
-    success: function(votes) {
-      $.each(votes, function(x, vote){
+    success: function success(votes) {
+      $.each(votes, function (x, vote) {
         if (vote.user_id == window.user_id && vote.park_id == window.park_id) {
           if (vote.choice == 1) {
             $("#upvote_" + vote.review_id).addClass("upvote");
@@ -17,80 +19,78 @@ $(function (){
     }
   });
 
-  $('.upvote-arrow').on('click', function(event) {
+  $('.upvote-arrow').on('click', function (event) {
     event.preventDefault();
 
     $.ajax({
       method: 'GET',
       url: '/api/v1/review_votes',
-      success: function(votes) {
+      success: function success(votes) {
         function checkVoteExists(vote) {
           return vote.user_id == window.user_id && vote.review_id == event.target.getAttribute("review_id");
         }
         // if vote doesn't exist, new vote created
         if (votes.find(checkVoteExists) === undefined) {
-          let newUpvote = {
+          var newUpvote = {
             choice: 1,
             park_id: window.park_id,
             review_id: event.target.getAttribute("review_id"),
             user_id: window.user_id,
             created_at: Date(),
-            updated_at: Date(),
+            updated_at: Date()
           };
 
           $.ajax({
             method: 'POST',
             url: '/api/v1/review_votes',
             data: newUpvote,
-            success: function(newVote) {
-              let current_score_id = ("#review_score_" + event.target.getAttribute("review_id"));
-              $(current_score_id).load(`/parks/${window.park_id} ${current_score_id}`, function() { });
+            success: function success(newVote) {
+              var current_score_id = "#review_score_" + event.target.getAttribute("review_id");
+              $(current_score_id).load('/parks/' + window.park_id + ' ' + current_score_id, function () {});
               $("#upvote_" + event.target.getAttribute("review_id")).addClass("upvote");
             }
           });
-
         } else if (votes.find(checkVoteExists).choice == 1) {
           $("#upvote_" + event.target.getAttribute("review_id")).removeClass("upvote");
-          let current_score_id = ("#review_score_" + event.target.getAttribute("review_id"));
+          var current_score_id = "#review_score_" + event.target.getAttribute("review_id");
           $.ajax({
             method: 'PUT',
             url: '/api/v1/review_votes/' + votes.find(checkVoteExists).id,
             data: { choice: 0 },
-            success: function(changeVote) {
-              $(current_score_id).load(`/parks/${window.park_id} ${current_score_id}`, function() { });
+            success: function success(changeVote) {
+              $(current_score_id).load('/parks/' + window.park_id + ' ' + current_score_id, function () {});
             },
-            error: function() {
+            error: function error() {
               alert("Vote was not changed");
             }
           });
-
         } else if (votes.find(checkVoteExists).choice === 0) {
           $("#downvote_" + event.target.getAttribute("review_id")).removeClass("downvote");
           $("#upvote_" + event.target.getAttribute("review_id")).addClass("upvote");
-          let current_score_id = ("#review_score_" + event.target.getAttribute("review_id"));
+          var current_score_id = "#review_score_" + event.target.getAttribute("review_id");
           $.ajax({
             method: 'PUT',
             url: '/api/v1/review_votes/' + votes.find(checkVoteExists).id,
             data: { choice: 1 },
-            success: function(changeVote) {
-              $(current_score_id).load(`/parks/${window.park_id} ${current_score_id}`, function() { });
+            success: function success(changeVote) {
+              $(current_score_id).load('/parks/' + window.park_id + ' ' + current_score_id, function () {});
             },
-            error: function() {
+            error: function error() {
               alert("Vote was not changed");
             }
           });
         } else if (votes.find(checkVoteExists).choice == -1) {
           $("#downvote_" + event.target.getAttribute("review_id")).removeClass("downvote");
           $("#upvote_" + event.target.getAttribute("review_id")).addClass("upvote");
-          let current_score_id = ("#review_score_" + event.target.getAttribute("review_id"));
+          var current_score_id = "#review_score_" + event.target.getAttribute("review_id");
           $.ajax({
             method: 'PUT',
             url: '/api/v1/review_votes/' + votes.find(checkVoteExists).id,
             data: { choice: 1 },
-            success: function(changeVote) {
-              $(current_score_id).load(`/parks/${window.park_id} ${current_score_id}`, function() { });
+            success: function success(changeVote) {
+              $(current_score_id).load('/parks/' + window.park_id + ' ' + current_score_id, function () {});
             },
-            error: function() {
+            error: function error() {
               alert("Vote was not changed");
             }
           });
@@ -99,86 +99,82 @@ $(function (){
     });
   });
 
-  $('.downvote-arrow').on('click', function(event) {
+  $('.downvote-arrow').on('click', function (event) {
     event.preventDefault();
 
     $.ajax({
       method: 'GET',
       url: '/api/v1/review_votes',
-      success: function(votes) {
+      success: function success(votes) {
         function checkVoteExists(vote) {
           return vote.user_id == window.user_id && vote.review_id == event.target.getAttribute("review_id");
         }
         // if vote doesn't exist, new vote created
         if (votes.find(checkVoteExists) === undefined) {
-          let newDownvote = {
+          var newDownvote = {
             choice: -1,
             park_id: window.park_id,
             review_id: event.target.getAttribute("review_id"),
             user_id: window.user_id,
             created_at: Date(),
-            updated_at: Date(),
+            updated_at: Date()
           };
           $.ajax({
             method: 'POST',
             url: '/api/v1/review_votes',
             data: newDownvote,
-            success: function(newVote) {
-              let current_score_id = ("#review_score_" + event.target.getAttribute("review_id"));
-              $(current_score_id).load(`/parks/${window.park_id} ${current_score_id}`, function() { });
+            success: function success(newVote) {
+              var current_score_id = "#review_score_" + event.target.getAttribute("review_id");
+              $(current_score_id).load('/parks/' + window.park_id + ' ' + current_score_id, function () {});
               $("#downvote_" + event.target.getAttribute("review_id")).addClass("downvote");
             }
           });
-
         } else if (votes.find(checkVoteExists).choice == -1) {
-          let current_score_id = ("#review_score_" + event.target.getAttribute("review_id"));
+          var current_score_id = "#review_score_" + event.target.getAttribute("review_id");
           $("#downvote_" + event.target.getAttribute("review_id")).removeClass("downvote");
           $.ajax({
             method: 'PUT',
             url: '/api/v1/review_votes/' + votes.find(checkVoteExists).id,
             data: { choice: 0 },
-            success: function(changeVote) {
-              $(current_score_id).load(`/parks/${window.park_id} ${current_score_id}`, function() { });
+            success: function success(changeVote) {
+              $(current_score_id).load('/parks/' + window.park_id + ' ' + current_score_id, function () {});
             },
-            error: function() {
+            error: function error() {
               alert("Vote was not changed");
             }
           });
-
         } else if (votes.find(checkVoteExists).choice === 0) {
-          let current_score_id = ("#review_score_" + event.target.getAttribute("review_id"));
+          var current_score_id = "#review_score_" + event.target.getAttribute("review_id");
           $("#upvote_" + event.target.getAttribute("review_id")).removeClass("upvote");
           $("#downvote_" + event.target.getAttribute("review_id")).addClass("downvote");
           $.ajax({
             method: 'PUT',
             url: '/api/v1/review_votes/' + votes.find(checkVoteExists).id,
             data: { choice: -1 },
-            success: function(changeVote) {
-              $(current_score_id).load(`/parks/${window.park_id} ${current_score_id}`, function() { });
+            success: function success(changeVote) {
+              $(current_score_id).load('/parks/' + window.park_id + ' ' + current_score_id, function () {});
             },
-            error: function() {
+            error: function error() {
               alert("Vote was not changed");
             }
           });
         } else if (votes.find(checkVoteExists).choice == 1) {
-          let current_score_id = ("#review_score_" + event.target.getAttribute("review_id"));
+          var current_score_id = "#review_score_" + event.target.getAttribute("review_id");
           $("#upvote_" + event.target.getAttribute("review_id")).removeClass("upvote");
           $("#downvote_" + event.target.getAttribute("review_id")).addClass("downvote");
           $.ajax({
             method: 'PUT',
             url: '/api/v1/review_votes/' + votes.find(checkVoteExists).id,
             data: { choice: -1 },
-            success: function(changeVote) {
-              $(current_score_id).load(`/parks/${window.park_id} ${current_score_id}`, function() { });
+            success: function success(changeVote) {
+              $(current_score_id).load('/parks/' + window.park_id + ' ' + current_score_id, function () {});
             },
-            error: function() {
+            error: function error() {
               alert("Vote was not changed");
             }
           });
         }
-
       }
     });
   });
-
 });
